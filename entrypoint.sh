5dd51ad430712408494c30ef596cbb1356e5a5f6
xxaxxelxx <x@axxel.net>
2016-01-20 13:58:16 +0100

    updated

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,10 @@
 CFGDIR="/etc/lighttpd"
 
 if [ -n "$TAGET_SERVER" ]; then
-    sed -i "s/\<SERVER\>/$TAGET_SERVER/g" /etc/lighttpd/lighttpd.conf
+    sed -i "s/\<SERVER\>/$TARGET_SERVER/g" $CFGDIR/lighttpd.conf
 fi
 if [ -n "$TAGET_PORT" ]; then
-    sed -i "s/\<PORT\>/$TAGET_PORT/g" /etc/lighttpd/lighttpd.conf
+    sed -i "s/\<PORT\>/$TARGET_PORT/g" $CFGDIR/lighttpd.conf
 fi
 
 lighttpd -D -f /etc/lighttpd/lighttpd.conf
